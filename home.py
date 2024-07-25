96e67f2e812be61d222b7bfaaa35dd9751f92502
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-07-25 21:33:21 +0700

    Change the size of lottie animation

--- home.py
+++ home.py
@@ -1,9 +1,10 @@
 import streamlit as st
-from st_pages import Page, show_pages, add_page_title
+from st_pages import Page, show_pages
 from streamlit_lottie import st_lottie
 
 st.title('Panen Dividen')
-st_lottie('https://lottie.host/632869fc-4f0f-4707-84ff-00c73c591eed/QEtuVsuLs5.json')
+st_lottie(animation_source='https://lottie.host/632869fc-4f0f-4707-84ff-00c73c591eed/QEtuVsuLs5.json',
+          height=200)
 
 show_pages(
     [
