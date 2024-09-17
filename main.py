060fdd639268cf235b3e9bfcc466762ae1eb0b95
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-09-17 14:17:20 +0700

    moved the location of the stock picker apps in the menu

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
 page_home = st.Page('home.py', title='Home', icon='🪙')
+page_screener = st.Page('apps/screener/stock_picker.py', title='Stock Picker', icon='💸')
 page_porto = st.Page('apps/porto/porto_overview.py', title='Portfolio Overview', icon='💰')
 page_history = st.Page('apps/history/history_overview.py', title='Historical Overview', icon='🧭')
-page_screener = st.Page('apps/screener/stock_picker.py', title='Stock Picker', icon='💸')
 
 pages = st.navigation(
     {
         'Home': [page_home],
-        'Apps': [page_porto, page_history, page_screener]
+        'Apps': [page_screener, page_porto, page_history]
     }
 )
 pages.run()
