23e5d15aaf3df4a9b27bb94cf0db02c1ab2388c6
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-10-11 14:01:31 +0700

    add function to plot dividend history

--- harvest/plot.py
+++ harvest/plot.py
@@ -78,3 +78,33 @@ def plot_pe_distribution(df, pe):
     )
    
     return pes_dist+x_zero
+
+
+def plot_dividend_history(div_df):
+
+    # aggregate to yearly basis for stock that paid interim during the year
+    dividend_year_df = div_df.copy()
+    dividend_year_df['year'] = div_df['date'].apply(lambda x: int(x.split('-')[0]))
+    yearly = dividend_year_df.groupby('year')['adjDividend'].sum().to_frame().reset_index()
+
+    # fill in the blank for the year when they do not pay dividend
+    start_year = yearly.loc[0, 'year']
+    end_year = yearly.loc[len(yearly)-1, 'year']
+
+    years = list(range(start_year, end_year + 1))
+    df_temp = pd.DataFrame({'year': years, 'value': [0]*len(years)})
+    df_train = pd.merge(df_temp, yearly, on='year', how='left')
+    df_train = df_train.fillna(0)
+    
+    # plot the dividend green if it is increased from last year, and red if it is decreased
+    df_train['inc'] = df_train['adjDividend'].shift(1) - df_train['adjDividend']
+    div_bar = alt.Chart(df_train).mark_bar().encode(
+        alt.X('year:N'),
+        alt.Y('adjDividend'),
+        color=alt.condition(alt.datum['inc'] > 0, alt.value('#ff796c'), alt.value('#008631'))
+    ).properties(
+        height=450,
+        width=600
+    )
+
+    return div_bar
