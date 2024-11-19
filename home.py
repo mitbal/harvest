ccd1a9c6257ccaea263bcbf75dc6c7a65c45caf7
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-11-19 15:21:17 +0700

    allow unsafe html in readme

--- home.py
+++ home.py
@@ -13,7 +13,7 @@ st_lottie(animation_source='https://lottie.host/632869fc-4f0f-4707-84ff-00c73c59
 
 with open('README.md', 'r') as f:
     desc = f.read()
-st.markdown(desc)
+st.markdown(desc, unsafe_allow_html=True)
 
 if 'porto_file' not in st.session_state:
     st.session_state['porto_file'] = 'EMPTY'
