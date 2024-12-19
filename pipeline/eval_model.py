edd44a01d3e948f8c9c3f97289863d2b726e49a4
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-12-19 15:22:17 +0700

    add function to evaluate single stock in eval pipeline

--- pipeline/eval_model.py
+++ pipeline/eval_model.py
@@ -7,6 +7,8 @@ import vectorbt as vbt
 
 from sklearn.ensemble import GradientBoostingClassifier
 
+import harvest.data as hd
+
 def eval_all():
 
     with open('data/features.pkl', 'rb') as f:
@@ -23,26 +25,34 @@ def eval_all():
     stock_list = feat_stats[(feat_stats['avg_pe'] > 0) & (feat_stats['avg_value'] > 10_000_000_000)]['stock'].values.tolist()
 
     ret_dict = {}
-    pred_dict = {}
     for stock in tqdm(stock_list):
         feat_df = feat_dict[stock]
         test_df = feat_df[test_start_date:test_end_date]
-        X = test_df.loc[:, test_df.columns != 'flag'].replace([np.inf, -np.inf], np.nan, inplace=False).fillna(0)
-        pred = gbc.predict(X)
-        pred_dict[stock] = pred
-
-        try:
-            entry = (pred == -1)
-            exits = (pred == 1)
-            pf = vbt.Portfolio.from_signals(test_df['close'], entry, exits, freq='d')
-            ret = pf.stats()
-            ret_dict[stock] = ret
-            print(stock, ret['Total Return [%]'])
-        except Exception as e:
-            print(f'error {stock}: {e}')
+        
+        stats = eval_single_stock(gbc, test_df)
+        ret_dict[stock] = stats
     
     with open('data/return_gbm.pkl', 'wb') as f:
         pickle.dump(ret_dict, f)
+
+
+def eval_single_stock(model, test_df):
+
+    X = test_df.loc[:, test_df.columns != 'flag'].replace([np.inf, -np.inf], np.nan, inplace=False).fillna(0)
+    pred = model.predict(X)
+
+    entry = (pred == -1)
+    exits = (pred == 1)
+    model_stats = hd.calc_return(test_df['close'], entry, exits)
+    model_stats['real_alpha'] = model_stats['Total Return [%]'] - model_stats['Benchmark Return [%]']
+
+    entry = (test_df['flag'] == -1)
+    exits = (test_df['flag'] == 1)
+    ideal_stats = hd.calc_return(test_df['close'], entry, exits)
+
+    model_stats['ideal_alpha_diff'] = ideal_stats['Total Return [%]'] - model_stats['Total Return [%]']
+    return model_stats
+
 
 if __name__ == '__main__':
 
