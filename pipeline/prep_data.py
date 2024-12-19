40c221e4bdf6e0fb83ea417c5fe3cf24be972978
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-12-19 14:24:29 +0700

    add feature stats function in prep data pipeline

--- pipeline/prep_data.py
+++ pipeline/prep_data.py
@@ -23,13 +23,16 @@ def compute_all():
     val_table = compute_valuation(stock_list, price_dict, fin_dict)
     val_table.to_csv('data/valuation.csv', index_label='stock')
 
+    feat_stats = compute_feature_stats(feat_dict)
+    feat_stats.to_csv('data/feat_stats.csv', index_label='stock')
+
 
 def compute_features(stock_list, price_dict, fin_dict):
 
     feat_dict = {}
     for stock in tqdm(stock_list):
         print(f'processing {stock}')
-        prices = price_dict[stock][['date', 'open', 'high', 'low', 'close']].sort_values('date').reset_index(drop=True).set_index('date')
+        prices = price_dict[stock][['date', 'open', 'high', 'low', 'close', 'volume']].sort_values('date').reset_index(drop=True).set_index('date')
 
         # calculate technical feature
         try:
@@ -42,14 +45,37 @@ def compute_features(stock_list, price_dict, fin_dict):
             pe_history = pe_history.set_index('date')['pe'].to_frame()
             pe_history.index = pe_history.index.astype('str')
 
-            labels = hd.make_labels(prices['close'], threshold=0.1)
+            labels = hd.make_labels(prices['close'], threshold=0.15)
             abc = labels.to_frame()
             abc.columns = ['flag']
 
             feat_dict[stock] = rsi.join(super_trend).join(bbands).join(stochrsi).join(prices).join(pe_history).join(abc)
+        
         except Exception as e:
             print(f'error {stock}: {e}')
+    
+    return feat_dict
 
+
+def compute_feature_stats(feat_dict):
+
+    stats_dict = {}
+    stock_list = list(feat_dict.keys())
+    for stock in stock_list:
+
+        feat_df = feat_dict[stock]
+        stats = {}
+        stats['count_entry_labels'] = (feat_df['flag'] == -1).sum()
+        stats['avg_value'] = (feat_df['volume']*feat_df['close']).mean()
+        stats['avg_pe'] = feat_df['pe'].mean()
+
+        feat_df['daily_return'] = feat_df['close'] / feat_df['close'].shift(1) - 1
+        stats['avg_daily_return'] = feat_df['daily_return'].mean()
+
+        stats_dict[stock] = stats
+    
+    return pd.DataFrame(stats_dict).transpose()
+    
 
 def compute_valuation(stock_list, price_dict, fin_dict):
 
