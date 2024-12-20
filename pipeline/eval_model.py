2a1a9cd208e2454117f27199cfccaa9886df3d56
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-12-20 16:48:02 +0700

    combine valuation and feature for eval shortlist

--- pipeline/eval_model.py
+++ pipeline/eval_model.py
@@ -15,20 +15,28 @@ def eval_all():
         feat_dict = pickle.load(f)
     
     model_dict = {
-        'gbc': 'data/gbc.pkl',
-        'lgbm': 'data/lgbm.pkl'
+        'lgbm': 'data/lgbm.pkl',
+        'xgb': 'data/xgb.pkl'
     }
 
     test_start_date = '2024-01-01'
     test_end_date = '2024-12-31'
-    feat_stats = pd.read_csv('data/feat_stats.csv')
-    stock_list = feat_stats[(feat_stats['avg_pe'] > 0) & (feat_stats['avg_value'] > 10_000_000_000)]['stock'].values.tolist()
+    feat_stats_df = pd.read_csv('data/feat_stats.csv')
+    val_df = pd.read_csv('data/valuation.csv')
+
+    att_df = feat_stats_df.merge(val_df, on='stock')
+    stock_list = att_df[(att_df['avg_value'] > 10_000_000_000) & (att_df['current_pe'] > 0) & (att_df['target'] > 0.03) & (att_df['current_pe'] < 30)]['stock'].values.tolist()
+
     for model_name, model_path in model_dict.items():
 
         with open(model_path, 'rb') as f:
             model = pickle.load(f)
 
         ret_dict = eval_single_model(model, feat_dict, stock_list, test_start_date, test_end_date)
+        ret_df = pd.DataFrame(ret_dict).transpose()
+        print(f'{model_name} total_return: {ret_df["Total Return [%]"].sum()}')
+        print(f'{model_name} real alpha: {ret_df["real_alpha"].sum()}')
+        print(f'{model_name} ideal alpha diff: {ret_df["ideal_alpha_diff"].sum()}')
         
         with open(f'data/return_{model_name}.pkl', 'wb') as f:
             pickle.dump(ret_dict, f)
@@ -44,14 +52,13 @@ def eval_single_model(model, feat_dict, stock_list, start_date, end_date):
         
         stats = eval_single_stock(model, test_df)
         ret_dict[stock] = stats
-        print(f'{stock} alpha {stats["real_alpha"]:.2f}')
     return ret_dict
 
 
 def eval_single_stock(model, test_df):
 
     X = test_df.loc[:, test_df.columns != 'flag'].replace([np.inf, -np.inf], np.nan, inplace=False).fillna(0)
-    pred = model.predict(X)
+    pred = model.predict(X) - 1
 
     entry = (pred == -1)
     exits = (pred == 1)
