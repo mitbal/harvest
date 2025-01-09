00475fd89891ef716bfa5bebc57fa771c6bda803
M Iqbal Tawakal <mit.iqi@gmail.com>
2025-01-09 13:59:39 +0700

    add calendar apps to harvest main page

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ page_home = st.Page('home.py', title='Home', icon='🪙')
 page_screener = st.Page('apps/screener/stock_picker.py', title='Stock Picker', icon='💸')
 page_porto = st.Page('apps/porto/porto_overview.py', title='Portfolio Overview', icon='💰')
 page_history = st.Page('apps/history/history_overview.py', title='Historical Overview', icon='🧭')
+page_calendar = st.Page('apps/calendar/calendar.py', title='Dividend Calendar', icon='📅')
 
 pages = st.navigation(
     {
         'Home': [page_home],
-        'Apps': [page_screener, page_porto, page_history]
+        'Apps': [page_screener, page_calendar, page_porto, page_history]
     }
 )
 pages.run()
