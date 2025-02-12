ca3eaa0412903684449b50d180a8bdc2eaaf5924
M Iqbal Tawakal <mit.iqi@gmail.com>
2025-02-12 14:30:44 +0700

    add footer and feature list

--- home.py
+++ home.py
@@ -31,7 +31,43 @@ with open('README.md', 'r') as f:
 with cols[1]:
     st.markdown(desc, unsafe_allow_html=True)
 
-if 'porto_file' not in st.session_state:
-    st.session_state['porto_file'] = 'EMPTY'
-if 'history_file' not in st.session_state:
-    st.session_state['history_file'] = 'EMPTY'
+##### Features breakdown
+
+st.divider()
+
+with open('style.html', 'r') as f:
+    style = f.read()
+st.html(style)
+
+with open('feature.html', 'r') as f:
+    feature = f.read()
+st.html(feature)
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+# text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+# text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ♥️ and lots of ☕ by <a style='display: block; text-align: center;' href="https://github.com/mitbal" target="_blank">mitochondrion</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
