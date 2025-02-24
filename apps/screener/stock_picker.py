73d05ebc3f3a2fc85813fffee6b070cd0c6622a1
M Iqbal Tawakal <mit.iqi@gmail.com>
2025-02-24 10:30:39 +0700

    make set page config the first command in stock picker

--- apps/screener/stock_picker.py
+++ apps/screener/stock_picker.py
@@ -11,11 +11,12 @@ from datetime import date, datetime, timedelta
 import harvest.plot as hp
 import harvest.data as hd
 
-st.title('Jajan Saham')
 try:
     st.set_page_config(layout='wide')
 except Exception as e:
     print('Set Page config has been called before')
+
+st.title('Jajan Saham')
 
 api_key = os.environ['FMP_API_KEY']
 redis_url = os.environ['REDIS_URL']
