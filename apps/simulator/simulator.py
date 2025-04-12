de8f5029da03bffbdd6528f748083b5c54f58049
M Iqbal Tawakal <mit.iqi@gmail.com>
2025-04-12 14:02:50 +0700

    add compounding plot

--- apps/simulator/simulator.py
+++ apps/simulator/simulator.py
@@ -17,4 +17,22 @@ for i in range(num_year):
 
 returns += [investments[-1] * avg_yield]
 return_df = pd.DataFrame({'investment': investments, 'returns': returns})
+return_df['year'] = [f'Year {i+1:02d}' for i in range(len(return_df))]
 st.dataframe(return_df)
+
+import altair as alt
+
+investment_chart = alt.Chart(return_df).mark_bar().encode(
+    x=alt.X('year:O', title='Year'),
+    y=alt.Y('investment:Q', title='Investment')
+)
+
+return_chart = alt.Chart(return_df).mark_line(point=True).encode(
+    x=alt.X('year:O', title='Year'),
+    y=alt.Y('returns:Q', title='Returns'),
+    color=alt.value('#FA8072')
+)
+
+compound_chart = alt.layer(investment_chart, return_chart).resolve_scale(y='independent')
+
+st.altair_chart(compound_chart)
