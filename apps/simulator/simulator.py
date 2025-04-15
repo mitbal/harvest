ade6af67720721b29dd5c153ae50b0387a87628e
M Iqbal Tawakal <mit.iqi@gmail.com>
2025-04-15 13:14:20 +0700

    add initial dividend reinvestment simulation

--- apps/simulator/simulator.py
+++ apps/simulator/simulator.py
@@ -3,6 +3,8 @@ import pandas as pd
 import altair as alt
 import streamlit as st
 
+import harvest.data as hd
+
 
 st.title('Historical Dividend Growth Simulator')
 
@@ -103,3 +105,63 @@ combined_chart = alt.Chart(combined_df).mark_bar().encode(
 )
 
 st.altair_chart((combined_chart + compound_chart).resolve_scale(y='independent'))
+
+
+st.write('## Single Stock Dividend Reinvestment')
+stock_name = st.text_input(label='Stock Name', value='INDF.JK')
+start_year = st.number_input(label='Start Year', value=2014, min_value=2000, max_value=2024)
+end_year = st.number_input(label='End Year', value=2024, min_value=2000, max_value=2024)
+div_df = hd.get_dividend_history_single_stock(stock_name)
+# st.dataframe(div_df)
+
+price_df = hd.get_daily_stock_price(stock_name, start_from=f'{start_year}-01-01')
+# st.dataframe(price_df)
+
+activities = []
+cash = initial_value
+porto = []
+divs = []
+investments = []
+returns = []
+
+for y in range(start_year, end_year+1):
+
+    # if y == start_year:
+    # close_price = ['close'].iloc[-1]
+    buy_date = price_df[price_df['date'] >= f'{y}-01-01'].iloc[-1]
+    # st.write(buy_date)
+    close_price = buy_date['close']
+    
+    buy = cash / close_price / 100
+    porto += [int(buy)]
+    cash -= int(buy) * close_price * 100
+    activities.append(f'buy {int(buy)} lots of {stock_name} @ {close_price} at {buy_date["date"]}')
+
+    buy_date = price_df[price_df['date'] <= f'{y}-12-31'].iloc[0]
+    investments += [np.sum(porto) * buy_date['close'] * 100]
+    
+    div = div_df[(div_df['date'] >= f'{y}-01-01') & (div_df['date'] <= f'{y}-12-31')]['adjDividend'].sum() * np.sum(porto) * 100
+    cash += div
+    activities.append(f'receive dividend {div}')
+
+    returns += [div]
+
+st.write(activities)
+
+return_df = pd.DataFrame({'investment': investments, 'returns': returns})
+return_df['year'] = [f'Year {i}' for i in range(start_year, end_year+1)]
+st.dataframe(return_df[['year', 'investment', 'returns']], hide_index=True)
+
+investment_chart = alt.Chart(return_df).mark_bar().encode(
+    x=alt.X('year:O', title='Year'),
+    y=alt.Y('investment:Q', title='Investment')
+)
+
+return_chart = alt.Chart(return_df).mark_line(point=True).encode(
+    x=alt.X('year:O', title='Year'),
+    y=alt.Y('returns:Q', title='Returns'),
+    color=alt.value('#FA8072')
+)
+
+st.altair_chart((investment_chart + return_chart).resolve_scale(y='independent')
+                , use_container_width=True)
