d1390f2fefccac926f36e7b9af34e187e75fab94
M Iqbal Tawakal <mit.iqi@gmail.com>
2025-05-04 15:50:23 +0700

    add badge after successfully updating porto

--- apps/porto/porto_overview.py
+++ apps/porto/porto_overview.py
@@ -503,3 +503,4 @@ with st.expander('Future Projection', expanded=True):
 if st.user.is_logged_in:
     if st.sidebar.button('Update Porto', icon=':material/cloud_upload:'):
         update_user_portfolio(conn, st.session_state['porto_df'].to_dict(), st.user.email)
+        st.badge("Success", icon=":material/check:", color="green")
