ef29ad972d1ebf26c836e626caf962752a1d32fb
M Iqbal Tawakal <mit.iqi@gmail.com>
2025-06-07 13:42:23 +0700

    change the name for some apps

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import streamlit as st
 page_home = st.Page('home.py', title='Home', icon='🪙')
 page_screener = st.Page('apps/screener/stock_picker.py', title='Stock Picker', icon='💸')
 page_porto = st.Page('apps/porto/porto_overview.py', title='Portfolio Overview', icon='💰')
-page_history = st.Page('apps/history/history_overview.py', title='Historical Overview', icon='🧭')
+page_history = st.Page('apps/history/history_overview.py', title='Historical Breakdown', icon='🧭')
 page_calendar = st.Page('apps/calendar/calendar.py', title='Dividend Calendar', icon='📅')
 page_assistant = st.Page('apps/assistant/assistant.py', title='Financial Assistant', icon='🧑‍🏫')
-page_article = st.Page('apps/article/article.py', title='Article', icon='📰')
-page_simulator = st.Page('apps/simulator/simulator.py', title='Simulator', icon='🎮')
+page_article = st.Page('apps/article/article.py', title='Analysis Article', icon='📰')
+page_simulator = st.Page('apps/simulator/simulator.py', title='Compounding Simulator', icon='🎮')
 
 pages = st.navigation(
     {
