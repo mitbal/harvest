d94838ce4d560e14949fcf9a1a8173b4b54c836b
M Iqbal Tawakal <mit.iqi@gmail.com>
2025-07-26 14:56:23 +0700

    add logging to the article page

--- apps/article/article.py
+++ apps/article/article.py
@@ -1,9 +1,22 @@
+import logging
+
 import streamlit as st
+
+from harvest.utils import setup_logging
 
 try:
     st.set_page_config(layout='wide')
 except Exception as e:
     print('Set Page config has been called before')
+
+
+@st.cache_resource
+def get_logger(name, level=logging.INFO):
+
+    logger = setup_logging(name, level)
+    return logger
+logger = get_logger('article')
+
 
 # 1. as sidebar menu
 with st.sidebar:
@@ -13,6 +26,7 @@ with st.sidebar:
     st.html('<a href="/article?name=sido">Better (Call) Buy SIDO</a>')
     st.html('<a href="/article?name=myor">MYOR != Pucuk Harum</a>')
     st.html('<a href="/article?name=bubble">Newton Bizarre (Financial) Adventure</a>')
+    st.html('<a href="/article?name=bjtm">BJ * (TM + BR) = Cuan???</a>')
 
 if len(st.query_params) == 0:
     st.title('Article')
@@ -23,3 +37,4 @@ article_name = st.query_params['name']
 
 with open(f'articles/{article_name}/{article_name}.md', 'r') as f:
     st.markdown(f.read())
+    logger.info(f'opening article {article_name}')
