ece36bc4739c89adc544ca2336b38db76cf410d2
M Iqbal Tawakal <mit.iqi@gmail.com>
2025-09-04 10:43:59 +0700

    add trakteer button on sidebar

--- main.py
+++ main.py
@@ -19,9 +19,16 @@ pages = st.navigation(
                  page_article,
                  page_simulator,
                  page_porto,
+                 page_viz,
                 #  page_history,
                 #  page_trading
                  ]
     }
 )
+
+with st.sidebar:
+    st.html(f'Support me on<br/>'+'<a href="https://trakteer.id/mitbal" target="_blank"><img id="wse-buttons-preview" src="https://cdn.trakteer.id/images/embed/trbtn-red-1.png?date=18-11-2023" height="40" style="border:0px;height:40px;" alt="Trakteer Saya"></a> ')
+    st.markdown('[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://buymeacoffee.com/mitbal)')
+    st.divider()
 pages.run()
+
