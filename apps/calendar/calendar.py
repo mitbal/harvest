90e610f90061bf77df6ecc3478eb8ed4cefe2ca0
M Iqbal Tawakal <mit.iqi@gmail.com>
2025-09-25 14:00:53 +0700

    add single month view in calendar apps

--- apps/calendar/calendar.py
+++ apps/calendar/calendar.py
@@ -5,6 +5,7 @@ import logging
 import calendar
 
 import redis
+import lesley
 import pandas as pd
 import streamlit as st
 
@@ -100,63 +101,81 @@ if sl == 'JKSE':
 else:
     show_next_year = False
 
-cal = hp.plot_dividend_calendar(df, show_next_year=False, sl=sl)
-st.altair_chart(cal)
+view_control = st.sidebar.segmented_control('Select View', ['Full Year', 'Single Month'], default='Full Year')
+if view_control == 'Single Month':
+    months = list(calendar.month_name)
+    select_month = st.sidebar.selectbox('Select Month', months[1:])
 
-idx = 1
-for i in range(3):
+if view_control == 'Full Year':
+    cal = hp.plot_dividend_calendar(df, show_next_year=False, sl=sl)
+    st.altair_chart(cal)
 
-    row = st.container()
-    row_cols = row.columns(4)
+    idx = 1
+    for i in range(3):
 
-    for j in range(4):
+        row = st.container()
+        row_cols = row.columns(4)
 
-        month_df = df[df.date.dt.month == idx].copy().reset_index(drop=True)
-        month_df.sort_values(by='yield', ascending=False, inplace=True)
+        for j in range(4):
 
-        month_df['rank'] = list(range(1, len(month_df)+1))
-        month_df['div_yield'] = month_df['yield'].apply(lambda x: f'{x:2.2f}%')
-        month_df['ex_date'] = month_df['date'].dt.strftime('%d %b')
-        month_df['url_link'] = month_df['symbol'].apply(lambda x: f'https://panendividen.com/stock_picker?stock={x}')
-        if sl == 'JKSE':
-            month_df['symbol'] = month_df['symbol'].apply(lambda x: x[:-3])
-        month_df.rename(columns={'symbol': 'stock',
-                                 'adjDividend': 'dividend'}, inplace=True)
+            month_df = df[df.date.dt.month == idx].copy().reset_index(drop=True)
+            month_df.sort_values(by='yield', ascending=False, inplace=True)
 
-        row_cols[j].write(f'{calendar.month_name[idx]}')
-        row_cols[j].dataframe(
-            hide_index=True,
-            column_config={
-                'url_link': st.column_config.LinkColumn(
-                    'Stock',
-                    help='The code of the stock',
-                    max_chars=10,
-                    display_text=r"https://panendividen\.com/stock_picker\?stock=([A-Z]+(?:\.JK)?)"
-                ),
-                'rank': st.column_config.NumberColumn(
-                    'Rank',
-                    help='Rank of the stock based on yield for this month',
-                    format='%d',
-                ),
-                'ex_date': st.column_config.TextColumn(
-                    'Ex Date',
-                    help='Ex-dividend date',
-                ),
-                'div_yield': st.column_config.TextColumn(
-                    'Yield',
-                    help='Dividend yield',
-                ),
-                'dividend': st.column_config.NumberColumn(
-                    'Dividend',
-                    help='Dividend amount',
-                    format='%.01f',
-                ),
-                'price': st.column_config.NumberColumn(
-                    'Price',
-                    help='Stock price',
-                    format='localized'
-                ),
-            },
-            data=month_df[['rank', 'url_link', 'ex_date', 'div_yield', 'dividend', 'price']], height=210)
-        idx += 1
-    
+            month_df['rank'] = list(range(1, len(month_df)+1))
+            month_df['div_yield'] = month_df['yield'].apply(lambda x: f'{x:2.2f}%')
+            month_df['ex_date'] = month_df['date'].dt.strftime('%d %b')
+            month_df['url_link'] = month_df['symbol'].apply(lambda x: f'https://panendividen.com/stock_picker?stock={x}')
+            if sl == 'JKSE':
+                month_df['symbol'] = month_df['symbol'].apply(lambda x: x[:-3])
+            month_df.rename(columns={'symbol': 'stock',
+                                    'adjDividend': 'dividend'}, inplace=True)
+
+            row_cols[j].write(f'{calendar.month_name[idx]}')
+            row_cols[j].dataframe(
+                hide_index=True,
+                column_config={
+                    'url_link': st.column_config.LinkColumn(
+                        'Stock',
+                        help='The code of the stock',
+                        max_chars=10,
+                        display_text=r"https://panendividen\.com/stock_picker\?stock=([A-Z]+(?:\.JK)?)"
+                    ),
+                    'rank': st.column_config.NumberColumn(
+                        'Rank',
+                        help='Rank of the stock based on yield for this month',
+                        format='%d',
+                    ),
+                    'ex_date': st.column_config.TextColumn(
+                        'Ex Date',
+                        help='Ex-dividend date',
+                    ),
+                    'div_yield': st.column_config.TextColumn(
+                        'Yield',
+                        help='Dividend yield',
+                    ),
+                    'dividend': st.column_config.NumberColumn(
+                        'Dividend',
+                        help='Dividend amount',
+                        format='%.01f',
+                    ),
+                    'price': st.column_config.NumberColumn(
+                        'Price',
+                        help='Stock price',
+                        format='localized'
+                    ),
+                },
+                data=month_df[['rank', 'url_link', 'ex_date', 'div_yield', 'dividend', 'price']], height=210)
+            idx += 1
+
+else:
+    st.write(select_month)
+    cols = st.columns(2)
+    month_index = months.index(select_month)
+
+    labels = df.apply(lambda x: f"{x['date'].strftime('%d %b')}: {x['symbol']} ({x['yield']:2.2f}%)", axis=1)
+    month_cal = lesley.month_plot(df['date'], df['yield'], labels=labels, month=month_index, show_date=True, width=500)
+    cols[0].altair_chart(month_cal)
+
+    month_df = df[df.date.dt.month == month_index].copy().reset_index(drop=True)
+    month_df.sort_values(by='yield', ascending=False, inplace=True)
+    cols[1].dataframe(month_df)
